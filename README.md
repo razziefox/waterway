5ead698a3f78d435681b1c7469a6209e2d5d3bae
florencio <57515252+razzie-dev@users.noreply.github.com>
2022-05-31 17:00:09 -0700

    Ver 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# waterway
- A simple reminder to drink water
+![logo]
+A simple reminder to drink water developed in the Godot Engine.
+
+# Downloads
+You may download the application through these storefronts:
+* [itch.io](https://razzie-dev.itch.io/waterway/)
+
+# Building
+To build, you'll need Godot Engine 3.4.4 which can be downloaded [here](https://github.com/godotengine/godot/releases/tag/3.4.4-stable).
+
+Once you've downloaded and setup the IDE, just simply open the project in the IDE and you're ready to go!
+
+# Credits
+* Godot by [Godot Engine contributors](https://godotengine.org/)
+* PICO-8 Font by [Lexaloffle](https://www.lexaloffle.com/)
+
+[logo]: logo.png
---
+++ scenes/application.gd
@@ -0,0 +1,62 @@
+extends Control
+
+# sets time variable to represent an hour
+var time = 10
+#3600
+
+func _ready():
+	
+	show_window()
+	
+	# sets and starts timer on load up
+	$Timer.set_wait_time(time)
+	$Timer.start()
+	
+	pass
+
+# function to hide the window away from the user
+func hide_window():
+	
+	# sets window size based on panel size
+	OS.set_window_size(Vector2($Panel.rect_size))
+	
+	# sets position away from view
+	OS.set_window_position(Vector2(0-OS.window_size.x, 0-OS.window_size.y))
+	
+	# sets and starts timer
+	$Timer.set_wait_time(time)
+	$Timer.start()
+	
+	pass
+
+# function to show the window
+func show_window():
+	
+	# sets window size based on panel size
+	OS.set_window_size(Vector2($Panel.rect_size))
+	
+	# sets position to the bottom right on the screen
+	OS.set_window_position(Vector2(OS.get_screen_size().x-$Panel.rect_size.x, OS.get_screen_size().y-($Panel.rect_size.y+80)))
+	
+	OS.set_window_always_on_top(true)
+	
+	OS.request_attention()
+	
+	pass
+
+# function for when timer is set off
+func _on_Timer_timeout():
+	
+	# shows window using show_window() function
+	show_window()
+	
+	pass
+
+
+func _on_Button_pressed():
+	
+	# hides window using hide_window() function
+	hide_window()
+	print("hello")
+	
+	pass # Replace with function body.
